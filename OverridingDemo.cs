c9d911c28dee28f2dda1af89eaf4f891254b1db3
geethasamynathan <geethasamynathan2011@gmail.com>
2023-02-10 14:57:26 +0530

    Adding Abstractdemoconsoleapp

---
+++ OverridingDemo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass_Demo
+{
+    class Parent
+    {
+        public virtual void aa()
+        {
+            Console.WriteLine(" aa from from Parent Class");
+        }
+        public void bb()
+        {
+
+            Console.WriteLine("bbb from Parent");
+        }
+    }
+
+    internal class OverridingDemo:Parent
+
+    {
+        public override void aa()
+        {
+            base.aa();
+            Console.WriteLine(" aa Overriden in Child class ");
+        }
+
+        public void cc()
+        {
+            Console.WriteLine("cc from Child");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace AbstractClass_Demo
+{
+    interface Isample
+    {
+        //private int y = 900;
+         void dd();
+    }
+    abstract class One
+    {
+    public void ff()
+        {
+            Console.WriteLine("FF from Abstract One");
+        }
+    }
+
+    abstract class Sample:One
+    {
+        private int y = 900;
+        public void aa()
+        {
+            Console.WriteLine(" AA from Abstract class");
+            Console.WriteLine(" Y avlue in abstract class"+y);
+        }
+
+        public abstract void bb();
+
+
+    }
+
+    class Example : Sample
+    {
+
+        public override void bb()
+        {
+            Console.WriteLine(" BB from Abstarct class implementation given in Example class");
+        }
+
+
+        public void cc()
+        {
+            Console.WriteLine(" CC From Example Class");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //   Example example=new Example();
+        //    example.aa();
+        //    example.bb();
+        //    example.cc();
+        //    example.ff();
+
+            OverridingDemo overridingDemo=new OverridingDemo();
+            overridingDemo.aa();
+            overridingDemo.bb();
+            overridingDemo.cc();
+            
+        }
+    }
+}
